68bc084e6c7c5610b4ce38f6b9b6a603547a9a8b
hkpanghal <112403425+hkpanghal@users.noreply.github.com>
2023-12-20 16:02:02 +0530

    added page4

--- script.js
+++ script.js
@@ -34,3 +34,49 @@ function p2CardAnim(){
     })
 }
 p2CardAnim()
+
+function p4DynamicCrsr(){
+    let dcimg = document.querySelector("#dcimg")
+    document.querySelectorAll(".dmove").forEach(function(elem){
+        
+        elem.addEventListener("mouseenter",function(){
+
+            dcimg.setAttribute("src",elem.children[0].getAttribute("src"))
+            gsap.to("#dynamic-crsr",{
+                display:"block",
+                opacity:1,
+                
+            })
+
+            gsap.to("#page4",{
+                backgroundColor:elem.getAttribute("data"),
+                duration:1,
+            })
+        })
+        elem.addEventListener("mouseleave",function(){
+
+            gsap.to("#dynamic-crsr",{
+                display:"none",
+                opacity:0
+            })
+
+            gsap.to("#page4",{
+                backgroundColor:"#dadada5f",
+            })
+        })
+        elem.addEventListener("mousemove",function(dets){
+       
+
+            
+             gsap.to("#dynamic-crsr",{
+                 x:dets.clientX,
+                 y:dets.clientY,
+                 duration:0.5,
+     
+             })
+         })
+    })
+
+   
+}
+p4DynamicCrsr()
